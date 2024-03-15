ad77f8e5f1877aeaa2173cd005d1e1b10c5ca1e5
Dan Koskei <qimtaid@gmail.com>
2024-03-15 05:44:42 +0300

    Complete lab

---
+++ index.js
@@ -0,0 +1,51 @@
+function shout(string) {
+    // todo
+  }
+
+  function shout(string) {
+    return string;
+  }
+
+  "Hello!".toUpperCase(); // 'HELLO!'
+
+  function shout(string) {
+    return string.toUpperCase();
+  }
+
+function whisper(string){
+    return string.toLowerCase();
+}
+
+
+function logShout(string){
+    console.log("HELLO");
+}
+
+function logWhisper(string){
+    console.log("hello");
+}
+
+
+
+
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+      return "I can't hear you!";
+    } else if (string === string.toUpperCase()){
+      return "YES INDEED!";
+    } else if ("Let\'s have dinner"){
+        return "I would love to!";
+    }
+    
+  }
+  
+ 
+  console.log(sayHiToHeadphonedRoommate("Hello")); 
+  console.log(sayHiToHeadphonedRoommate("YES INDEED")); 
+  console.log(sayHiToHeadphonedRoommate("I would love to")); 
+ 
+
+  
+  
+ 
